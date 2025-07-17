f1dc7c829616ea6abf7945c8719737115e96e30a
jiae <kga9002@naver.com>
2025-07-17 17:57:49 +0900

    [UPDATE] 퀴즈 풀이

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: '구구단 화면'),
     );
   }
 }
@@ -29,18 +29,27 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  // void _incrementCounter() {
+  //   setState(() {
+  //     _counter++;
+  //   });
+  // }
 
-  void _DecrementCounter() {
-    setState(() {
-      _counter--;
-    });
+  // void _DecrementCounter() {
+  //   setState(() {
+  //     _counter--;
+  //   });
+  // }
+
+  String _getMultiply(int dan) {
+    String text = "";
+    for (int i = 1; i <= 9; i++) {
+      text += "$dan X $i = ${dan * i}\n";
+    }
+
+    return text;
   }
 
   @override
@@ -49,34 +58,41 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        centerTitle: true,
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                for (var i = 1; i <= 9; i++)
+                  Text(_getMultiply(i), style: TextStyle(fontSize: 8)),
+              ],
             ),
+            // Text(
+            //   '$_counter',
+            //   style: Theme.of(context).textTheme.headlineMedium,
+            // ),
           ],
         ),
       ),
-      floatingActionButton: Row(
-        children: [
-          FloatingActionButton(
-            onPressed: _incrementCounter,
-            tooltip: 'Increment',
-            child: const Icon(Icons.add),
-          ),
-          Padding(padding: EdgeInsets.all(10)),
-          FloatingActionButton(
-            onPressed: _DecrementCounter,
-            tooltip: 'Decrement',
-            child: const Icon(Icons.remove),
-          ),
-        ],
-      ),
+      // floatingActionButton: Row(
+      //   children: [
+      //     FloatingActionButton(
+      //       onPressed: _incrementCounter,
+      //       tooltip: 'Increment',
+      //       child: const Icon(Icons.add),
+      //     ),
+      //     Padding(padding: EdgeInsets.all(10)),
+      //     FloatingActionButton(
+      //       onPressed: _DecrementCounter,
+      //       tooltip: 'Decrement',
+      //       child: const Icon(Icons.remove),
+      //     ),
+      //   ],
+      // ),
     );
   }
 }
